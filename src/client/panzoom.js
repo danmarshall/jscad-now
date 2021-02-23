920d07394ce0ae2c174d795b4de9d877eb626e3b
Dan Marshall <danmar@microsoft.com>
2021-02-22 23:58:36 -0800

    use locals

--- src/client/panzoom.js
+++ src/client/panzoom.js
@@ -4,22 +4,22 @@ const panSpeed = 1
 const zoomSpeed = 0.08
 
 module.exports = function (scope) {
-    const { camera, controls } = scope;
-    if (scope.rotateDelta[0] || scope.rotateDelta[1]) {
-        const updated = orbit.rotate({ controls, camera, speed: rotateSpeed }, scope.rotateDelta)
+    const { camera, controls, panDelta, rotateDelta, zoomDelta } = scope;
+    if (rotateDelta[0] || rotateDelta[1]) {
+        const updated = orbit.rotate({ controls, camera, speed: rotateSpeed }, rotateDelta)
         scope.rotateDelta = [0, 0]
         scope.updateOrbitControls(updated.controls)
     }
 
-    if (scope.panDelta[0] || scope.panDelta[1]) {
-        const updated = orbit.pan({ controls, camera, speed: panSpeed }, scope.panDelta)
+    if (panDelta[0] || panDelta[1]) {
+        const updated = orbit.pan({ controls, camera, speed: panSpeed }, panDelta)
         scope.panDelta = [0, 0]
         camera.position = updated.camera.position
         camera.target = updated.camera.target
     }
 
-    if (scope.zoomDelta) {
-        const updated = orbit.zoom({ controls, camera, speed: zoomSpeed }, scope.zoomDelta)
+    if (zoomDelta) {
+        const updated = orbit.zoom({ controls, camera, speed: zoomSpeed }, zoomDelta)
         scope.updateOrbitControls(updated.controls)
         scope.zoomDelta = 0
     }
